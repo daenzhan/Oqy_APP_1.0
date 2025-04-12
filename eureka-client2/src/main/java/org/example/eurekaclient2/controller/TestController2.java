b1dde8c2f96bfd6fc038f4c95e47de76bcf24803
Arusha <storio.tommy@gmail.com>
2025-04-12 21:12:47 +0500

    two test ureka-clients - worked!

--- eureka-client2/src/main/java/org/example/eurekaclient2/controller/TestController2.java
+++ eureka-client2/src/main/java/org/example/eurekaclient2/controller/TestController2.java
@@ -1,14 +1,14 @@
-package org.example.eurekaclient.conrtoller;
+package org.example.eurekaclient2.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/main")
-public class TestController {
-    @GetMapping("/test")
-    public String test (){
-        return "tefgfgst";
+@RequestMapping("/hello")
+public class TestController2 {
+    @GetMapping("/world")
+    public String name (){
+        return "hello world!!!";
     }
 }
